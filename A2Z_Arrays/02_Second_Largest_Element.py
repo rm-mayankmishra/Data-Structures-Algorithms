c8c5384eabb35adb8583ae9787cbe5aea326c391
rm-mayankmishra <rm.mayankmishra@gmail.com>
2026-04-18 17:40:23 +0530

    Implement function to find second largest element

--- A2Z_Arrays/02_Second_Largest_Element.py
+++ A2Z_Arrays/02_Second_Largest_Element.py
@@ -1,1 +1,21 @@
+def second_largest_element(nums):
+    if len(nums) < 2:
+        return -1
 
+    largest = float('-inf')
+    second_largest = float('-inf')
+
+    for num in nums:
+        if num > largest:
+          second_largest = largest
+          largest = num
+
+        elif num > second_largest and num != largest:
+            second_largest = num
+
+    if second_largest == float('-inf'):
+        return -1
+
+    return second_largest
+
+    
