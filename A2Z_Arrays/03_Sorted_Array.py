ddc70eb17d7ca2ff8b0a4ccc42905ad8686ae3f0
rm-mayankmishra <rm.mayankmishra@gmail.com>
2026-04-18 20:25:49 +0530

    Implement sorted array check function
    Add function to check if an array is sorted.

--- A2Z_Arrays/03_Sorted_Array.py
+++ A2Z_Arrays/03_Sorted_Array.py
@@ -1,1 +1,10 @@
+arr = [2, 3, 4, 5, 7, 8, 9]
 
+def sorted_array(arr):
+    for i in range(1, len(arr)):
+        if arr[i] < arr[i-1]:
+            return False
+    return True
+
+# TC : O(n)
+# SC : O(1)
