b2fd0714ff522754525a3b391228abbfcba1dd11
rm-mayankmishra <rm.mayankmishra@gmail.com>
2026-04-19 20:50:53 +0530

    Add remove_duplicates function to filter unique values
    Implement function to remove duplicates from a sorted list.

--- A2Z_Arrays/04_Remove_Duplicates.py
+++ A2Z_Arrays/04_Remove_Duplicates.py
@@ -1,1 +1,14 @@
+nums = [1, 2, 2, 3, 3, 4, 7, 9, 11]
 
+def remove_duplicates(nums):
+    if len(nums) == 0:
+      return 0
+
+    i = 0 
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+    return i + 1        
+
+    
