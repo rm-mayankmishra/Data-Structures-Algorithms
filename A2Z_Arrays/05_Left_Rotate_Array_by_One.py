e0690c25fd08e5c2bd8465d45413be746f0f27bd
rm-mayankmishra <rm.mayankmishra@gmail.com>
2026-04-21 21:23:48 +0530

    Add left_rotate_by_one function
    Implement function to left rotate an array by one position.

--- A2Z_Arrays/05_Left_Rotate_Array_by_One.py
+++ A2Z_Arrays/05_Left_Rotate_Array_by_One.py
@@ -1,1 +1,11 @@
+def left_rotate_by_one(nums):
+    if len(nums) == 0:
+        return nums
 
+    first = nums[0]
+
+    for i in range(1, len(nums)):
+        nums[i - 1] = nums[i]
+
+    nums[-1] = first
+    return nums
