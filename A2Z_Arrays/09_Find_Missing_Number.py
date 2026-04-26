2d6f4b9fd1525471099a3509ad734f21fa6518d4
rm-mayankmishra <rm.mayankmishra@gmail.com>
2026-04-26 20:35:15 +0530

    Implement missing number finder in Solution class

--- A2Z_Arrays/09_Find_Missing_Number.py
+++ A2Z_Arrays/09_Find_Missing_Number.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def missingNumber(self, nums):
+        n = len(nums)
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        number = expected_sum - actual_sum
+        return number
