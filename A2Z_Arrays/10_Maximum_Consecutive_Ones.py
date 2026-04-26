edf6287a8cd559840f77a9a5a2c2fb7275467209
rm-mayankmishra <rm.mayankmishra@gmail.com>
2026-04-26 20:40:17 +0530

    Implement function to find maximum consecutive ones

--- A2Z_Arrays/10_Maximum_Consecutive_Ones.py
+++ A2Z_Arrays/10_Maximum_Consecutive_Ones.py
@@ -1,1 +1,12 @@
+def maximum_consecutive_ones(nums):
+    count = 0
+    max_count = 0
 
+    for num in nums:
+          if num == 1:
+              count += 1
+              max_count = max(max_count, count)
+
+          else:
+              count = 0
+    return max_count
