37b6ee288ddac2c41e8ec892939e8af950875a75
rm-mayankmishra <rm.mayankmishra@gmail.com>
2026-05-18 23:25:27 +0530

    Implement bubble sort algorithm

--- A2Z_Sorting/2_Bubble_Sort.py
+++ A2Z_Sorting/2_Bubble_Sort.py
@@ -1,1 +1,9 @@
+# Bubble Sort
 
+def bubble_sort(nums):
+    n = len(nums)
+    for i in range(n-1):
+        for j in range(n-1-i):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    return nums
